06e2c7f09a7b87635b6b248d8617002fc23edc1d
KALU56 <kalkidanbehailu55@gmail.com>
2025-03-02 23:26:02 +0300

    link to home and about

---
+++ my-app/app/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+       <link href='/about'>
+            go to about
+            </link>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
